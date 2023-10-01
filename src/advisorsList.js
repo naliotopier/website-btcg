824329ec05c2c6728a31eed872a7ee8876b8ec9b
Jimmy Phelan <james_phelan@brown.edu>
2023-10-01 17:29:43 -0400

    feat: updates fall 2023

--- src/advisorsList.js
+++ src/advisorsList.js
@@ -20,6 +20,27 @@ const advisorsList = [
     imageName: "salo-pic.jpeg",
     bio: "Matt graduated Brown University last May with a concentration in Neuroscience. Matt works as an analyst at ClearView Healthcare Partners in their New York City office. As a advisor to BTCG, Matt facilitates the development of internal infrastructure and provides insight on the consulting recruitment process.",
   },
+  {
+    id: 4,
+    fullName: "Gene Oh",
+    title: "Co-Founder",
+    imageName: "gene-pic.png",
+    bio: "Gene is a junior from Sharon, MA, pursuing a concentration in Computer Science with specializations in AI/ML and Data Science. Also a Head TA for the Brown Computer Science Department and a private writing tutor of three years, Gene has a demonstrated passion for teaching and working with others. Outside of the classroom, he is a member of the Brown Men's Rugby Team and enjoys hiking, swimming, and snowboarding.",
+  },
+  {
+    id: 5,
+    fullName: "Jonathan Palfy",
+    title: "Co-Founder",
+    imageName: "johnathan-pic.jpeg",
+    bio: "Jonathan is a junior from Marin County, CA, pursuing a concentration in Behavioral Decision Sciences and a certificate in Data Fluency. This interdisciplinary degree focuses on decision making principles in the fields of economics, psychology, political science, and data analysis. Jonathan has also written articles for the Intercollegiate Finance Journal and enjoys playing basketball or hiking in his free time.",
+  },
+  {
+    id: 6,
+    fullName: "Nicholas Alioto-Pier",
+    title: "Co-Founder",
+    imageName: "nico-pic.jpg",
+    bio: "Nicholas is a junior from San Francisco, CA. He is working on completing his Bachelor's and Master's in Computer Science through Brown's 4-year concurrent degree program. Nicholas is particularly interested in artificial intelligence and front-end developement. He previously worked as a Full Stack SWE Intern at MongoDB and a Front-End Intern at Whimstay, Inc. In his free time, Nicholas enjoys surfing, playing sudoku, and reading Supreme Court cases.",
+  },
 ];
 
 export default advisorsList;
